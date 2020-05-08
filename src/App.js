bc30430f3c567b7313ff1f9a1aadf0a76bfde9da
Hamza Alalach <hamzaalalach@gmail.com>
2020-05-08 22:27:09 +0100

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import NavBar from './components/NavBar';
+import { useAuth0 } from "./react-auth0-spa";
 import './App.css';
+import { Router, Route, Switch } from "react-router-dom";
+import Profile from "./components/Profile";
+import history from "./utils/history";
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
+  const { loading } = useAuth0();
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router history={history}>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route exact path='/' />
+          <PrivateRoute path='/profile' component={Profile} />
+        </Switch>
+      </Router>
     </div>
   );
 }
